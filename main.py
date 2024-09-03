02e48b42ded6b068660617c2666bfd5fe310d16f
KacpiGGG <gkacper629@gmail.com>
2024-09-03 19:26:49 +0200

    Create main.py
    Kod1

---
+++ main.py
@@ -0,0 +1,11 @@
+import random 
+slowa={
+"LOL": "odpowiedź na coś zabawnego",
+"CRINGE": "coś dziwnego lub wstydliwego",
+"ROFL": "odpowiedź na żart",
+"SHEESH": "lekka dezaprobata",
+"CREEPY": "straszny, złowieszczy",
+"AGGRO": "stać się agresywnym/zły",  
+}
+slowo=input("Podaj słowo,a dowiesz się co znaczy!")
+print(slowa[slowo])
